04b13c6237d20e6b6e293843b85a1f52f8d0b848
willwsxu <willwsxu@gmail.com>
2017-03-06 17:18:41 -0600

    add submatrix class, basic coding

---
+++ src/Mar17SubMatrix.java
@@ -0,0 +1,29 @@
+
+import java.util.Scanner;
+
+/*
+ *
+ */
+class Matrix
+{
+    int[][] mat;
+    Matrix(int N) {
+        mat = new int[N][N];
+    }
+    
+    void scanValue()
+    {
+        
+    }
+}
+class Mar17SubMatrix {
+    static Scanner scan = new Scanner(System.in);
+    
+    public static void main(String[] args)
+    {        
+        int TC = scan.nextInt();
+        for (int i=0; i<TC; i++) {
+        
+        }
+    }
+}
