d551f896ffb30ef697b100d1f616618e06467fff
willwsxu <willwsxu@gmail.com>
2017-03-07 19:59:50 -0600

    improve sumdistance perforamnce

--- src/Mar17SumDistance.java
+++ src/Mar17SumDistance.java
@@ -1,5 +1,7 @@
 
 import static java.lang.System.out;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -28,26 +30,38 @@ class Mar17SumDistance {
         int d3 = alldist.get(ts-4).get(s)+alldist.get(2).get(t-3);
         return d>d3?d3:d;
     }
-    Mar17SumDistance(int N)
+    Mar17SumDistance(int N, boolean biglytest)
     {
         int [] dist1 = new int[N-1];  // weight from node 1 to 2, etc
         int [] dist2 = new int[N-2];  // weight from node 1 to 3, etc
         int [] dist3 = new int[N-3];  // weight from node 1 to 4, etc
-        fillArray(dist1);
-        fillArray(dist2);
-        fillArray(dist3);
+        if (biglytest) {
+            for (int i=0; i<dist1.length; i++)
+                dist1[i]=i+1;
+            for (int i=0; i<dist2.length; i++)
+                dist2[i]=i+2;
+            for (int i=0; i<dist3.length; i++)
+                dist3[i]=i+3;        
+        } else {
+            fillArray(dist1);
+            fillArray(dist2);
+            fillArray(dist3);
+        }
         long total=0;
         // smallest distance between note s and t
         List<List<Integer>> alldist = new ArrayList<>();
+        Instant start = Instant.now();
         // case t-s=1
-        List<Integer> dist = new ArrayList<>();
+        List<Integer> dist = new ArrayList<>(N-1);
         for (int i=0; i<N-1; i++) {
             dist.add(dist1[i]);
             total += dist1[i];
         }
         alldist.add(dist);
+        Instant end = Instant.now();
+        out.println("calc first 1 row takes "+ChronoUnit.MICROS.between(start, end));
         // case t-s=2
-        dist = new ArrayList<>();
+        dist = new ArrayList<>(N-2);
         for (int i=0; i<N-2; i++) {
             int d = dist1[i]+dist1[i+1];
             if ( d>dist2[i])
@@ -57,7 +71,7 @@ class Mar17SumDistance {
         }
         alldist.add(dist);
         // case t-s=3
-        dist = new ArrayList<>();
+        dist = new ArrayList<>(N-3);
         for (int i=0; i<N-3; i++) {
             int d = alldist.get(1).get(i)+alldist.get(0).get(i+2);
             int d2 = alldist.get(0).get(i)+alldist.get(1).get(i+1);
@@ -69,6 +83,10 @@ class Mar17SumDistance {
             dist.add(d);
         }
         alldist.add(dist);
+        // free memory
+        dist1=null;
+        dist2=null;
+        dist3=null;
         // case t-s from 4 to N-1
         for (int ts=4; ts <=N-1; ts++) {
             dist = new ArrayList<>();
@@ -78,6 +96,10 @@ class Mar17SumDistance {
                 dist.add(d);
             }
             alldist.add(dist);
+            if ( ts>7 )
+                alldist.get(ts-5).clear(); // clear memory as we only need first 3 rows and last three rows
+            if ( ts %1000==0 )
+                out.println(ts);
         }
         /*
         for(int i=0; i<N-1; i++) {
@@ -86,13 +108,17 @@ class Mar17SumDistance {
         }*/
         out.println(total);
     }
-    public static void main(String[] args)
-    {
-        int TC = scan.nextInt();
+    static void autoTest()
+    {        
+        int TC = scan.nextInt();  // between 1 and 10^4
         for (int i=0; i<TC; i++) {
             int N = scan.nextInt();  // between 4 and 10^5
-            new Mar17SumDistance(N);
+            new Mar17SumDistance(N, false);
         }        
+    }
+    public static void main(String[] args)
+    {
+        new Mar17SumDistance(100000, true);
     }    
 }
 /*
