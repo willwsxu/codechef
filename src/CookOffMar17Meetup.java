93427f9b61519ff2ba00bfb49ec62e869d788934
willwsxu <willwsxu@gmail.com>
2017-03-23 17:28:26 -0500

    meetup reference impl (not done)

--- src/CookOffMar17Meetup.java
+++ src/CookOffMar17Meetup.java
@@ -3,9 +3,11 @@ import static java.lang.System.out;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
+import java.util.Set;
 
 
 class CookOffMar17Meetup {
@@ -163,52 +165,147 @@ class CookOffMar17Meetup {
                 out.println("Node "+(i)+": "+adjList.get(i));
             }
         }
+        void solve(Graph ga, Graph gb) 
+        {
+            while (true) {
+                int node = ga.queryClique(gb);
+                while (node>=0) {
+                    ga.trimNoneNeighbors(node);
+                    gb.trimNoneNeighbors(ga.match[node]);
+                    node = ga.queryClique(gb);
+                }
+                ga.print();
+                gb.print();
+                if (node==-1)
+                    return;
+                if (ga.complete()) {
+                    return;
+                }
+                node = gb.queryIndependent(ga);
+                boolean trim=false;
+                while (node>=0) {
+                    trim=true;
+                    ga.trimNeighbors(gb.match[node]);
+                    gb.trimNeighbors(node);
+                    node = gb.queryIndependent(ga);
+                }
+                gb.print();
+                ga.print();
+                if (node==-1)
+                    return;
+                if (gb.complete()) {
+                    return;
+                }
+                if ( !trim ) {
+                    out.println("C No");
+                    return;
+                }
+            }
+        }
     }
-    Graph   ga, gb;
+    class Graph2{
+        Map<String, HashSet<String>> adjList = new HashMap<>(MAX_NODE);
+        Set<String> destination = new HashSet<>(MAX_NODE);
+        Graph2(int N, int M, int k)
+        {
+            String []city = readLine();
+            for (int i=0; i<N; i++) {
+                if (i<k)
+                    destination.add(city[i]);
+                adjList.put(city[i], new HashSet<>());
+            }
+            for (int i=0; i<M; i++) {
+                String[] road = readLine();
+                if (road.length==2) {
+                    adjList.get(road[0]).add(road[1]);  // add adjacent nodes
+                    adjList.get(road[1]).add(road[0]);  // add adjacent nodes
+                }
+            }        
+        }
+        void reduce(String s, boolean bNeighbor)
+        {
+            // remove all nodes if it is neighbor or not
+            adjList.keySet().removeIf(x->!x.equals(s)&&adjList.get(s).contains(s)==bNeighbor);
+            // remove all linked nodes that is no longer part of the graph
+            adjList.values().forEach(v->v.removeIf(x->!adjList.containsKey(x)));
+        }
+        // true : done
+        public boolean queryClique(Graph2 other)
+        {
+            int size = adjList.size();
+            for (String s: adjList.keySet()) {
+                if ( !destination.contains(s))
+                    continue;
+                if (adjList.get(s).size()*2<size) {
+                    String answer = query("A", s);
+                    if ( other.destination.contains(answer)) {
+                        out.println("queryClique C Yes");
+                        return true;
+                    }
+                    if ( size==1) {
+                        out.println("queryClique C No");
+                        return true;
+                    }
+                    reduce(s, false);
+                    other.reduce(s, false);
+                    return false;
+                }
+            }
+            return true;
+        }
+        public boolean queryIndependent(Graph2 other)
+        {
+            int size = adjList.size();
+            for (String s: adjList.keySet()) {
+                if ( !destination.contains(s))
+                    continue;
+                if (adjList.size()*2>=size) {
+                    String answer = query("B", s);
+                    if ( other.destination.contains(answer)) {
+                        out.println("queryIndependent C Yes");
+                        return true;
+                    }
+                    if ( size==1) {
+                        out.println("queryIndependent C No");
+                        return true;
+                    }
+                    reduce(s, true);
+                    other.reduce(s, true);
+                    return false;
+                }
+            }
+            return true;            
+        }
+        void solve(Graph2 ga, Graph2 gb)
+        {
+            while (true)
+            {
+                if (!ga.queryClique(gb))
+                    continue;
+                if (!ga.queryClique(gb))
+                    continue;
+                out.println("C No");
+                break;
+            }
+        }
+        void print()
+        {
+            out.println("dest: "+destination);
+            adjList.forEach((k,v)->out.println(k+": "+v));
+        }
+    }
+    Graph2   ga, gb;
     CookOffMar17Meetup()
     {
         int N = scan.nextInt();  // cities 1 and 1000
         int M = scan.nextInt();  // bi-directional roads
         int kA = scan.nextInt(); // # of city Alice visits, clique   
         int kB = scan.nextInt(); // # of city Bob visits, independent set 
-        ga = new Graph(N, M, kA);
-        gb = new Graph(N, M, kB);
-        //ga.print();
-        //gb.print();
-        while (true) {
-            int node = ga.queryClique(gb);
-            while (node>=0) {
-                ga.trimNoneNeighbors(node);
-                gb.trimNoneNeighbors(ga.match[node]);
-                node = ga.queryClique(gb);
-            }
-            ga.print();
-            gb.print();
-            if (node==-1)
-                return;
-            if (ga.complete()) {
-                return;
-            }
-            node = gb.queryIndependent(ga);
-            boolean trim=false;
-            while (node>=0) {
-                trim=true;
-                ga.trimNeighbors(gb.match[node]);
-                gb.trimNeighbors(node);
-                node = gb.queryIndependent(ga);
-            }
-            gb.print();
-            ga.print();
-            if (node==-1)
-                return;
-            if (gb.complete()) {
-                return;
-            }
-            if ( !trim ) {
-                out.println("C No");
-                return;
-            }
-        }
+        ga = new Graph2(N, M, kA);
+        gb = new Graph2(N, M, kB);
+        ga.print();
+        gb.print();
+        ga.solve(ga, gb);
     }
     
     public static void main(String[] args)
