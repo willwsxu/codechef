fa8d00ab5a751f55397a2a8af46779748ec9d197
ANDY-PROBOOK\Will <willwsxu@gmail.com>
2017-03-25 20:16:46 -0500

    revise bear species using graph dfs, pass one test

--- src/LunchMar17Species.java
+++ src/LunchMar17Species.java
@@ -1,162 +1,82 @@
 
 import static java.lang.System.out;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 class LunchMar17Species {
-    char [][]board;
-    
-    char bear(char [][]x, int i, int j)
+    // name each cell of the grid as node, from 0 to N*N-1
+    // translate n=i*N+j
+    List<List<Integer>> graph=new ArrayList<>(2501);
+    void add(String [] x, int current, int i, int j, int N)
     {
-        switch(x[i][j]) {
-            case '.':
-            case '?':
-                return 0;
-            default:
-                return x[i][j];
-        }
-    }     
-    char validate(char[][]x, int i, int j)
-    {
-        char neighbor=0;
-        if (i>0) {
-            char b = bear(x, i-1, j);
-            if (b>0) {
-                if (neighbor>0)
-                    return 0; // ? is between two different bears
-                if (b=='G')
-                    return 0;
-                neighbor=b;
-            }
-        }
-        if (i<x.length-1) {
-            char b = bear(x, i+1, j);
-            if (b>0) {
-                if (neighbor>0)
-                    return 0; // ? is between two different bears
-                if (b=='G')
-                    return 0;
-                neighbor=b;
-            }
-        }
-        if (j>0) {
-            char b = bear(x, i, j-1);
-            if (b>0) {
-                if (neighbor>0)
-                    return 0; // ? is between two different bears
-                if (b=='G')
-                    return 0;
-                neighbor=b;
-            }            
-        }
-        if ( j<x.length-1 ) {
-            char b = bear(x, i, j+1);
-            if (b>0) {
-                if (neighbor>0)
-                    return 0; // ? is between two different bears
-                if (b=='G')
-                    return 0;
-                neighbor=b;
-            }                        
-        }
-        return neighbor>0?neighbor:'?';
+        if ( x[i].charAt(j) == '.' )
+            return;
+        int next = (i)*N+j;
+        graph.get(current).add(next);
+        graph.get(next).add(current);
     }
-    void print()
-    {
-        for(char[] x: board)
-            out.println(Arrays.toString(x));
-    }
-    
-    int calcM(char [][]board, int i, int j, int m)
-    {
-        if (board[i][j] =='?') {
-            board[i][j]='X';
-            if (m>2)
-                m=2;
-        }else if (board[i][j] =='X') {
-            m=1;
+    boolean []visited;
+    void dfs(String [] x, int current, int N, StringBuilder build) {
+        int r = current/N;
+        int c = current%N;
+        build.append(x[r].charAt(c));
+        visited[current]=true;
+        for (int next: graph.get(current))
+        {
+            if (!visited[next])
+                dfs(x, next, N, build);
         }
-        return m;
-    }
-    int multiplier(char [][]board, int i, int j)
-    {
-        int N = board.length;
-        int m=3;
-        if ( i>0 )
-            m = calcM(board, i-1, j, m);
-        if ( i<N-1 )
-            m = calcM(board, i+1, j, m);
-        if ( j>0 )
-            m = calcM(board, i, j-1, m);
-        if ( j<N-1 )
-            m = calcM(board, i, j+1, m);
-        if (m != 3)
-            board[i][j]='X';
-        return m;
     }
     LunchMar17Species(String [] x)
     {
         int N = x.length;
-        board = new char[N][N];
+        for (int i=0; i<N*N; i++)
+            graph.add(new ArrayList<>());
         for (int i=0; i<N; i++) {
             for (int j=0; j<N; j++) {
-                board[i][j]=x[i].charAt(j);
+                if (x[i].charAt(j) =='.')
+                    continue;
+                int current = i*N+j;
+                if (i<N-1)
+                    add(x, current, i+1, j, N);
+                if (j<N-1)
+                    add(x, current, i, j+1, N);
             }
         }
-        //print();
-        boolean notdone=true;
-        while(notdone) {
-            notdone =false;
-            for (int i=0; i<N; i++) {
-                for (int j=0; j<N; j++) {
-                    switch(board[i][j]) {
-                        case '?':
-                            char b = validate(board, i, j);
-                            if (b==0) {
-                                out.println("0");
-                                //print();
-                                return;
-                            }
-                            notdone = board[i][j]!=b;
-                            board[i][j]=b;
-                            break;
-                    }                     
-                }
-            }
-            //print();
-        }
+        visited = new boolean[N*N];
         long total=1;
-        outerfor:
-        for (int i=0; i<N; i++) {
-            for (int j=0; j<N; j++) {
-                switch(board[i][j]) {
-                    case '?':
-                        total *= multiplier(board, i, j);
-                        if (total >= MOD)
-                            total -= MOD;
-                        break;
-                    case 'X':
-                        break;
-                    case 'G':
-                        if (i>0 && board[i-1][j] != '.') {
-                            total = 0;
-                            break outerfor;
-                        }
-                        if (i<N-1 && board[i+1][j] != '.') {
-                            total = 0;
-                            break outerfor;
-                        }
-                        if (j>0 && board[i][j-1] != '.') {
-                            total = 0;
-                            break outerfor;
-                        }
-                        if (j<N-1 && board[i][j+1] != '.') {
-                            total = 0;
-                            break outerfor;
-                        }
-                        break;
-                }
+        for (int i=0; i<N*N; i++)
+        {
+            if (visited[i])
+                continue;
+            visited[i]=true;
+            List<Integer> node = graph.get(i);
+            if (node.isEmpty()) {
+                int r = i/N;
+                int c = i%N;
+                if (x[r].charAt(c)=='?')
+                    total *=3;
+                if (total >= MOD)
+                    total -= MOD;
+                continue;
             }
+            StringBuilder build = new StringBuilder();
+            dfs(x, i, N, build);
+            String cells = build.toString();
+            boolean b=cells.indexOf('B')>=0;
+            boolean p=cells.indexOf('P')>=0;
+            if (cells.indexOf('G')>=0 || b&&p) {
+                total=0;
+                break;
+            }
+            if (b || p)
+                total *= 1;
+            else
+                total *= 2;
+            if (total >= MOD)
+                total -= MOD;
         }
         out.println(total);
     }
