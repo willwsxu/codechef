7888ebc792286d55878b0e85a8d50f0621e48d82
ANDY-PROBOOK\Will <willwsxu@gmail.com>
2017-03-28 23:01:36 -0500

    basic Haircut class

---
+++ src/LunchMar17Haircut.java
@@ -0,0 +1,18 @@
+
+import java.util.Scanner;
+
+public class LunchMar17Haircut {
+    
+    static Scanner scan = new Scanner(System.in);
+    public static void autoTest()
+    {
+        int TC = scan.nextInt();  // between 1 and 6
+        for (int i=0; i<TC; i++) {
+            int N = scan.nextInt();  // between 1 to 150,000
+            String []board = new String[N];
+            for (int j=0; j<N; j++) {
+                
+            }
+        }
+    }
+}
