b1dcff5542140717984ce8191bfab1435e5562af
willwsxu <willwsxu@gmail.com>
2017-04-12 09:18:17 -0500

    complete clique sssp, pass one test

--- src/Apr17CliqueDist.java
+++ src/Apr17CliqueDist.java
@@ -2,9 +2,11 @@
 import static java.lang.System.out;
 import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.HashSet;
 import java.util.List;
 import java.util.PriorityQueue;
 import java.util.Scanner;
+import java.util.Set;
 
 // single-source shortest paths
 class Apr17CliqueDist {
@@ -12,7 +14,6 @@ class Apr17CliqueDist {
     static Scanner scan = new Scanner(System.in);
     public static void main(String[] args)
     {        
-        //scan = codechef.CodeChef.getFileScanner("dishLife0417.txt");
         int TC = scan.nextInt();  // between 1 and 10
         for (int i=0; i<TC; i++) {
             int N = scan.nextInt();   // 2 ≤ K ≤ N ≤ 10^5
@@ -20,14 +21,14 @@ class Apr17CliqueDist {
             long X = scan.nextLong(); // 1 to 10^9
             int M = scan.nextInt();     // 1 to 10^5 new roads
             int s = scan.nextInt();
-            WeightedGraph g = new WeightedGraph(N);
+            SSSPclique sp = new SSSPclique(N, K, X, s-1);
             for (int j=0; j<M; j++) {
-                int v = scan.nextInt();
+                int v = scan.nextInt();  // index from 1
                 int w = scan.nextInt();
                 long wt = scan.nextLong();
-                g.addDirectEdge(new Edge(v-1, w-1, wt)); // index from 0
+                sp.addEdge(v-1, w-1, wt);
             }
-            SSSPclique sp = new SSSPclique(g, K, X, s-1);
+            sp.run();
             for (int k=0; k<N; k++)
                 out.print(sp.distTo(k)+" ");
             out.println();
@@ -94,13 +95,14 @@ class WeightedGraph {
     {
         int v = e.either(), w = e.other(v);
         adj[v].add(e);
-        adj[w].add(new Edge(w, v, e.weight()));
         E++;
+        //out.println(" edge from "+(v+1)+" to "+(w+1)+" w="+e.weight());
     }
 }
 
 // shortest path from s, clique 1 to k with same weight
 // vertex is 0 based
+// modified from Dijkstra algorithm
 class SSSPclique
 {
     class PQItem{
@@ -125,30 +127,31 @@ class SSSPclique
     }
     private final int   K; // 1 to vertices clique
     private final long  Kw; // weight between clique
+    private final int   s; // source node
     private final WeightedGraph g;
     private Edge[] edgeTo;
     private long[] distTo;
     private PriorityQueue<PQItem> pq;
-    SSSPclique(WeightedGraph g, int k, long w, int s)
+    
+    SSSPclique(int N, int k, long w, int s)
     {
-        this.g = g;
+        g = new WeightedGraph(N);
         K=k;
         Kw=w;
+        this.s=s;
         edgeTo = new Edge[g.V()];
         distTo = new long[g.V()];
         Comparator<PQItem> cmp = Comparator.comparingLong(a->a.weight);
         pq = new PriorityQueue<>(g.V(), cmp);
         for (int v = 0; v < g.V(); v++) {
-            if (v<K)
-                distTo[v] = w;
-            else
-                distTo[v] = Long.MAX_VALUE;
+            if (v<K && v!=s && s<K) {
+                g.addDirectEdge(new Edge(s, v, w));
+            }
+            distTo[v] = Long.MAX_VALUE;
         }
         distTo[s] = 0;
         
         pq.add(new PQItem(s, 0));
-        while (!pq.isEmpty())
-            relax(pq.poll().v);
     }
     
     private void relax(int v)
@@ -156,6 +159,7 @@ class SSSPclique
         for(Edge e : g.adj(v))
         {
             int w = e.to();
+            //out.println(" dist to v "+(v+1)+" is "+distTo[v]+" w="+w);
             if (distTo[w] > distTo[v] + e.weight())
             {
                 distTo[w] = distTo[v] + e.weight();
@@ -164,11 +168,54 @@ class SSSPclique
                     pq.remove(new Integer(w));
                 }
                 pq.add(new PQItem(w, distTo[w]));
+                //out.println("update dist to w "+(w+1)+" is "+distTo[w]);
             }
         }
     }
     public long distTo(int v) // standard client query methods
     {
         return distTo[v];
+    }
+    
+    Set<Integer> in = new HashSet<>();
+    public void addEdge(int v, int w, long wt)
+    {            
+        if ( v<K && s<K) {
+            g.addDirectEdge(new Edge(v, w, wt)); 
+        }
+        else if ( w<K && s<K) {
+            g.addDirectEdge(new Edge(w, v, wt)); 
+        }
+        else {
+            g.addDirectEdge(new Edge(v, w, wt));
+            g.addDirectEdge(new Edge(w, v, wt));                    
+        }
+        if ( s>= K) {
+            if ( v<K )
+                in.add(v);
+            else if ( w<K )
+                in.add(w);
+        }   
+    }
+    private void cliqueEdges()
+    {
+        //out.println("edges "+g.E());
+        if ( s>=K ) { // s is not in clique
+            for (int k: in) {
+                for (int j=0;j<K;j++) {
+                    if (k != j)
+                        g.addDirectEdge(new Edge(k, j, Kw));   
+                }
+            }
+        }
+        in.clear();// only need it once
+        //out.println("edges "+g.E());   
+    }
+
+    public void run()
+    {
+        cliqueEdges();
+        while (!pq.isEmpty())
+            relax(pq.poll().v);
     }
 }
