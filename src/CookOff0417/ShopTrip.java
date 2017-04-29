7ed8439c421d698b767d6c5459edca2bddc3b6ec
ANDY-PROBOOK\Will <willwsxu@gmail.com>
2017-04-29 12:52:14 -0500

    April cookoff review

--- src/CookOff0417/ShopTrip.java
+++ src/CookOff0417/ShopTrip.java
@@ -5,20 +5,20 @@ import static java.lang.Math.sqrt;
 import static java.lang.System.out;
 import java.util.Scanner;
 
-
+// Dynamic programming, dp, TSP
 class ShopTrip {
     
     double [][]dist;
-    int N, K;
-    int ingre[]; // binary
-    int all;
+    int N, K;       // N city (max 36), K ingredient
+    long ingre[];    // binary bit for ingredients of each city, max 12
+    long all;
     ShopTrip(long X[], long Y[], String ingre[], int K)
     {
         this.K = K;
         X[0]=Y[0]=0;
         N = X.length-1;
         dist = new double[N+1][N+1];
-        this.ingre = new int[ingre.length];
+        this.ingre = new long[ingre.length];
         for (int i=0; i<N; i++) {
             for (int j=i+1; j<=N; j++) {
                 dist[i][j]=sqrt((X[j]-X[i])*(X[j]-X[i])+(Y[j]-Y[i])*(Y[j]-Y[i]));
@@ -29,17 +29,17 @@ class ShopTrip {
             //out.println(Integer.toBinaryString(this.ingre[i]));
         }
         for (int i=0; i<K; i++) {
-            all |= (1<<i);
+            all |= (1L<<i);
         }
         //out.println(Integer.toBinaryString(all));
         recurse(0, 0.0, 0);
         if (distance != Double.MAX_VALUE)
-            out.println(distance);
+            out.println(String.format("%.9f", distance));
         else
             out.println(-1);
     }
     double distance=Double.MAX_VALUE;
-    void recurse(int start, double d, int ingre)
+    void recurse(int start, double d, long ingre)
     {
         if (ingre == all) {
             d += dist[start][0];// go back
@@ -58,7 +58,7 @@ class ShopTrip {
     static Scanner sc = new Scanner(System.in);
     public static void main(String[] args)
     {
-        //sc = codechef.ContestHelper.getFileScanner("shoptrip-t.txt");
+        sc = codechef.ContestHelper.getFileScanner("shoptrip-t.txt");
         int TC = sc.nextInt();  // 1 ≤ T ≤ 10
         for (int i=0; i<TC; i++) {
             int N = sc.nextInt();   // 1 ≤ N ≤ 36
--- src/CookOff0417/XorSum.java
+++ src/CookOff0417/XorSum.java
@@ -5,9 +5,8 @@ import static java.lang.System.out;
 import java.util.Scanner;
 
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * XOR sum of all segments of an integer sequence
+ * Medium, segment tree
  */
 
 
