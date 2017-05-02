8ef62bfac60330b62d5e3384eb26a4136592c417
willwsxu <willwsxu@gmail.com>
2017-05-02 09:29:57 -0500

    stable market refactor

--- src/Apr17StableMarket.java
+++ src/Apr17StableMarket.java
@@ -1,26 +1,29 @@
 
-import codechef.ContestHelper;
 import static java.lang.System.out;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+// Editorial IDEA: convert list of numbers 
 // SMARKET medium hard 
-class Apr17StableMarket {
+
+// slight improvement over brute force
+class BlockOrder
+{
     List<Integer> blocks=new ArrayList<>();        // precompute order of blocks
-    int blkIndex[];      // point index of blocks
+    int blkIndex[];      // point to index of blocks
     int blkPos[];        // position in its block, 1 mean firt number of the block
     int maxBlk=0;
     int minBlk=Integer.MAX_VALUE;
-    
-    Apr17StableMarket(int p[])
+
+    BlockOrder(int p[])
     {
         blkIndex = new int[p.length];
         blkPos = new int[p.length];
         int pos=1;
         int order=1;
         int lastP=p[0];
-        
+
         blkPos[0]=pos;
         blkIndex[0]=0;
         for (int i=1; i<p.length; i++)
@@ -46,11 +49,7 @@ class Apr17StableMarket {
         if ( order<minBlk )
             minBlk = order;
         if ( order>maxBlk )
-            maxBlk = order;
-        /*out.println(Arrays.toString(p));
-        out.println(Arrays.toString(blkIndex));
-        out.println(Arrays.toString(blkPos));
-        out.println(Arrays.toString(blocks));*/
+            maxBlk = order;            
     }
     int blocks(int n1, int n2, int ord)
     {
@@ -69,6 +68,12 @@ class Apr17StableMarket {
             if (blocks.get(i)>=ord)
                 ++blk;
         return blk;
+    }
+}
+class Apr17StableMarket {
+    
+    Apr17StableMarket(int p[])
+    {
     }
     int bruteforce(int p[], int n1, int n2, int ord)
     {
@@ -93,7 +98,7 @@ class Apr17StableMarket {
     static Scanner scan = new Scanner(System.in);
     public static void main(String[] args)
     {        
-        scan = ContestHelper.getFileScanner("stablemarket-t.txt");
+        scan = codechef.ContestHelper.getFileScanner("stablemarket-t.txt");
         int TC = scan.nextInt();  // between 1 and 5
         for (int i=0; i<TC; i++) {
             int N = scan.nextInt();   // 1 ≤ N, Q ≤ 10^5
@@ -101,7 +106,7 @@ class Apr17StableMarket {
             int prices[] = new int[N]; // stock prices, 1 to 10^6
             for (int j=0; j<N; j++)
                 prices[j] = scan.nextInt();
-            Apr17StableMarket mkt = new Apr17StableMarket(prices);
+            BlockOrder mkt = new BlockOrder(prices);
             for (int j=0; j<Q; j++) {
                 int n1 = scan.nextInt();
                 int n2 = scan.nextInt();
