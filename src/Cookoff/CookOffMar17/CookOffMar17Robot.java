f4e1e6d8d761308b858c186daee29f5417998ee1
willwsxu <willwsxu@gmail.com>
2017-05-04 12:26:58 -0500

    refactor safe robot to use shared Grid helper

--- src/Cookoff/CookOffMar17/CookOffMar17Robot.java
+++ src/Cookoff/CookOffMar17/CookOffMar17Robot.java
@@ -1,45 +1,19 @@
-package CookOffMar17;
-
+package Cookoff.CookOffMar17;
 
 import static java.lang.System.out;
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
+import java.util.Arrays;
 import java.util.Scanner;
 
 
+// safe robot, ROBOTG easy (or beginner)
 class CookOffMar17Robot {
         
     public String isSafe(int rows, int cols, String moves)
     {
-        int r=0, c=0;
-        int minR=0, minC=0, maxR=0, maxC=0;
-        for (int i=0; i<moves.length(); i++) {
-            switch(moves.charAt(i)){
-                case 'U':
-                    r--;
-                    break;
-                case 'D':
-                    r++;
-                    break;
-                case 'L':
-                    c--;
-                    break;
-                case 'R':
-                    c++;
-                    break;
-                default:
-                    return "unsafe";
-            }
-            if ( minR>r)
-                minR=r;
-            if ( maxR<r)
-                maxR=r;
-            if ( minC>c)
-                minC=c;
-            if (maxC<c)
-                maxC=c;
-        }
-        if (maxR-minR>=rows || maxC-minC>=cols)
+        int[]mb = codechef.GridHelper.movingBox(moves);
+        if ( mb[2]-mb[0]>=rows || mb[3]-mb[1]>=cols)
             return "unsafe";
         return "safe";
     }
@@ -58,7 +32,7 @@ class CookOffMar17Robot {
     }
     public static void main(String[] args)
     {
-        //scan = codechef.CodeChef.getFileScanner("robot.txt");
+        scan = codechef.ContestHelper.getFileScanner("robot0317.txt");
         //Instant start = Instant.now();
         autoTest();
         //Instant end = Instant.now();
