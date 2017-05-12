0680a0112bc7c420c3b622be4acc1ef4f7c92344
willwsxu <willwsxu@gmail.com>
2017-05-12 11:40:47 -0500

    ArrayList performance is better, pass 50% large test, 50% still TLE

--- src/SubArray.java
+++ src/SubArray.java
@@ -1,7 +1,12 @@
 
 import static java.lang.System.out;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Comparator;
+import java.util.List;
 import java.util.PriorityQueue;
 import java.util.Scanner;
 
@@ -58,10 +63,11 @@ class SubArray {
     }
     int head=0;
     int tail=0;
-    PriorityQueue<Integer> pq=new PriorityQueue<>(100000, Comparator.reverseOrder());
+    //PriorityQueue<Integer> pq=new PriorityQueue<>(100000, Comparator.reverseOrder());
     void cacheSum(StringBuilder sb)
     {
-        tail=N-K;
+        tail=N-K;        
+        SortedList pq = new SortedList(true);
         Asum = new int[N];
         for (int i=0; i<K; i++) {
             Asum[0] += A[i];  // store sum of next K elements
@@ -150,7 +156,8 @@ class SubArray {
     static Scanner sc = new Scanner(System.in);
     public static void main(String[] args)
     {      
-        //test();/*
+        //SortedList.perfTest(100000);
+        //test();
         int N=sc.nextInt();  // 1 ≤ N, K, P ≤ 10^5
         int K=sc.nextInt();
         int P=sc.nextInt();  // P=p.length
@@ -159,5 +166,93 @@ class SubArray {
             A[j]=sc.nextInt();  // 0 or 1
         String p=sc.next();
         new SubArray(A, K, p).solve();
+    }
+}
+
+// ArrayList is 25x faster than linkedlist for sorted list
+class SortedList
+{
+    private List<Integer> ls;
+    private PriorityQueue<Integer> pq;
+    Comparator<Integer> cmp;
+    SortedList(boolean reverseOrder)
+    {
+        if (reverseOrder)
+            cmp = Comparator.reverseOrder();
+        else
+            cmp = Comparator.naturalOrder();
+        ls = new ArrayList<>(10000);
+        //pq = new PriorityQueue<>(10000, cmp);
+    }
+    public boolean add(Integer e)
+    {
+        if (ls!=null)
+        {
+        int i=Collections.binarySearch(ls, e, cmp);
+        if (i<0)
+            i=-(i+1);
+        ls.add(i, e);
+        } else {
+            pq.add(e);
+        }
+        return true;
+    }
+    public int peek()
+    {
+        if (ls!=null)
+            return ls.get(0);
+        else
+            return pq.peek();
+    }
+    
+    public boolean remove(Integer e)
+    {
+        if (ls!=null) {
+        int i=Collections.binarySearch(ls, e, cmp);
+        if (i>=0) {
+            ls.remove(i);
+            return true;
+        }
+        } else {
+            pq.remove(e);
+        }
+        return false;
+    }
+    public static void test()
+    {
+        SortedList sll = new SortedList(true);
+        sll.add(10);
+        sll.add(20);
+        sll.add(30);
+        sll.add(5);
+        sll.add(15);
+        System.out.println(sll);
+        System.out.println(sll.peek());
+        sll.remove(new Integer(30));
+        System.out.println(sll.peek());
+        sll.add(4);
+        sll.remove(0);
+        System.out.println(sll);
+    }
+    public static void perfTest(int N)
+    {
+        Instant start = Instant.now();
+        SortedList sll = new SortedList(true);
+        for (int i=0; i<N; i++) {
+            sll.add(i%(N/3)+1);
+        }        
+        Instant mid = Instant.now();
+        out.println("usec "+ChronoUnit.MICROS.between(start, mid));
+        // when N=100000, LinkedList 11 sec, ArrayList .45 sec, PriorityQueue 15msec
+        
+        for (int i=0; i<N; i++) {
+            int a=i%(N/7)+1;
+            int d=i%(N/3)+1;
+            sll.add(a);
+            sll.remove(new Integer(d));
+        }
+        Instant end = Instant.now();   
+        out.println("usec "+ChronoUnit.MICROS.between(mid, end));   
+        // when N=100000, LinkedList 83 sec, ArrayList 1.3 sec, PriorityQueue 3.6 sec
     }
 }
