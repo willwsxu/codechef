3e65f93cc728e618c02d7974fd04d715a2e31002
willwsxu <willwsxu@gmail.com>
2017-05-17 17:31:39 -0500

    digit virus brute force

--- src/DigitVirus.java
+++ src/DigitVirus.java
@@ -1,26 +1,53 @@
 
 
 
+import static java.lang.Integer.max;
+import static java.lang.Integer.min;
+import static java.lang.Math.abs;
+import static java.lang.System.out;
+import java.util.Arrays;
 import java.util.Scanner;
 
 
 class DigitVirus {
-    static void virus(String v)
+    static void bruteforce(String v)
     {
-        char[] virus=v.toCharArray();
-        int max=0;
-        int maxPos[]=new int[v.length()];
-        for (int i=0; i<v.length(); i++) {
-            if (max<virus[i])
-                max=virus[i];
+        char[] s=v.toCharArray();
+        char[] ns=new char[s.length];
+        Arrays.fill(ns, '0');
+        int ans=0;
+        boolean change=false;
+        {
+            for (int i=0; i<v.length(); i++) {
+                // for each virus at i, check its +- 9 neighbors to see it it can mutate them
+                for (int j=max(0, i-9); j<min(i+9, s.length); j++) {
+                    if (s[i]-s[j]>=abs(i-j) && ns[j]<s[i]) {
+                        ns[j]=s[i];
+                        change=true;
+                    }
+                }
+            }
+            ans++;
+            out.println(Arrays.toString(ns));
+            s=ns;
         }
+        while (change);
+        out.println(ans);
+    }
+    static void test()
+    {
+        bruteforce("555755555");        // 3
+        bruteforce("311110000000000");  // 6
+        bruteforce("07788000744");      // 4
     }
     static Scanner sc = new Scanner(System.in);
     public static void main(String[] args)
     {
+        test();
+        /*
         int TC = sc.nextInt();  // between 1 and 3
         for (int i=0; i<TC; i++) {
             String s = sc.next(); //1 ≤ N ≤ 150,000
-        }
+        }*/
     }
 }
