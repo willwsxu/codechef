9b6562978b75d5c73923a31b0bd27a1fa54f1144
willwsxu <willwsxu@gmail.com>
2017-05-19 10:18:40 -0500

    digit virus add comments

--- src/DigitVirus.java
+++ src/DigitVirus.java
@@ -1,4 +1,14 @@
-
+/* Virus of strength 0 to 9 in a string e.g. "8761", a virus of higher strength
+ * can infect neighbor of lower strength if the difference of strength is no less than
+ * difference of distance, i.e. S[i] - S[j] >= |i - j|, infection takes a sec regardless
+ * of distance. e.g. in 1 sec, "87700010010"->"88788888811"
+ * output: seconeds needed to infect all virus into a single strength
+ * Editorial idea: bfs
+ * use queue to store all virus position, in each turn, output new strength in a new array
+ * and save the virus which changed strength into a queue
+ * additional performance boost, use a vis[] boolean to keep track of changes in each round
+ *  it helps to add virus to queue just once.
+*/
 
 
 import static java.lang.Integer.max;
@@ -13,7 +23,10 @@ import java.util.concurrent.ConcurrentLinkedQueue;
 // DIGVIRUS medium
 class DigitVirus {
     
-    // 
+    // qa stores current virus position, initially all
+    // qb store virus that changed strength, only these virus need to be processed again
+    // ns store updated strength, only if strength increases
+    // vis is used to ensure virus is added to qb only once
     static void bfs(String v)
     {
         Queue<Integer> qa=new ConcurrentLinkedQueue<>();
@@ -22,7 +35,7 @@ class DigitVirus {
         char[] ns=new char[s.length];
         for (int i=0; i<s.length; i++) {
             qa.add(i);
-            ns[i]=s[i];
+            ns[i]=s[i];  // initial state, edge test case
         }
         int vis[]=new int[s.length];
         Arrays.fill(vis, 0);  // an efficient to add only modified virus to qb once
@@ -30,7 +43,8 @@ class DigitVirus {
         while (true) {
             count++;
             while (!qa.isEmpty()) {
-                int i=qa.poll();                
+                int i=qa.poll();         
+                // only need to check virus within distance 9
                 for (int j=max(0, i-9); j<min(i+9, s.length); j++) {
                     if (s[i]-s[j]>=abs(i-j) && ns[j]<s[i]) {
                         ns[j]=s[i];
@@ -103,11 +117,11 @@ class DigitVirus {
         out.print("bfs ");        bfs("1"); 
         bruteforce("10");
         out.print("bfs ");        bfs("10"); 
+        bfs("87700010010");
     }
     static Scanner sc = new Scanner(System.in);
     public static void main(String[] args)
     {
-        //test();
         int TC = sc.nextInt();  // between 1 and 3
         for (int i=0; i<TC; i++) {
             String s = sc.next(); //1 ≤ N ≤ 150,000
