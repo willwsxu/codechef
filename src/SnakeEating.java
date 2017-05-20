ff09214988b0951bc0216998c9148b68059ac63c
ANDY-PROBOOK\Will <willwsxu@gmail.com>
2017-05-20 17:07:04 -0500

    Snake eating pass basic test

---
+++ src/SnakeEating.java
@@ -0,0 +1,133 @@
+
+import static java.lang.System.out;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
+import java.util.stream.IntStream;
+
+/*
+
+ */
+
+class SnakeEating {
+    Integer L[];
+    long prefix[];
+    int Q;
+    SnakeEating(Integer a[], int q)
+    {
+        L=a;
+        Arrays.sort(L, Comparator.reverseOrder());
+        Q=q;
+        prefix = new long[L.length];
+        prefix[0]=L[0];
+        for (int i=1; i<L.length; i++)
+            prefix[i]=prefix[i-1]+L[i];
+    }
+    // find postion that enough snakes can be eaten to grow snake from start to pos to length k
+    int binarySnake(int start, int pos, int step, int k)
+    {
+        //out.println("pos "+pos+" step="+step);
+        int next=pos+step;
+        long diff=prefix[next];
+        if (start>0)
+            diff -= prefix[start-1];
+        diff = k*(next-start+1)-diff;
+        if (diff>L.length-next-1) {
+            if (step==0)
+                return 0;
+            return binarySnake(start, pos, step/2, k);
+        }
+        else if (diff==L.length-next-1)
+            return next-start+1;
+        else {
+            if (step==0)
+                return 1;
+            return binarySnake(start, next, step/2, k);            
+        }
+    }
+    int query(int k)
+    {
+        int count=0;
+        int x=Arrays.binarySearch(L, k, Comparator.reverseOrder());
+        //out.println("bs "+x);
+        if (x<0) {
+            x = -(x+1);
+            count=x;
+        }else {
+            count = ++x;
+        }
+        // snake from x, eat snake at the end
+        if (x>=L.length-1)  // edge test case
+            return count;
+        int c=binarySnake(x, x, (L.length-x)/2, k);
+        //out.println("count "+count+" x="+x+" c="+c);
+        return count+c;
+    }
+    void query()
+    {
+        StringBuilder sb = new StringBuilder();
+        for (int i=0; i<Q; i++) {
+            int k=sc.nextInt(); // 1 ≤ Ki ≤ 109
+            sb.append(query(k));
+            sb.append("\n");
+        }
+        out.print(sb.toString());
+    }
+    static void test()
+    {
+        SnakeEating sn = new SnakeEating(new Integer[]{21, 9, 5, 8, 10}, 2);
+        /*out.println(Arrays.toString(sn.L));
+        out.println(Arrays.toString(sn.prefix));
+        out.println(sn.query(10));
+        out.println(sn.query(15));
+        sn = new SnakeEating(new Integer[]{1, 2, 3, 4, 5}, 2);
+        out.println(sn.query(100));// 0
+        out.println(sn.query(8));  // 1
+        out.println(sn.query(6));  // 2
+        out.println(sn.query(1));  // 5
+        out.println(sn.query(2));  // 4
+        out.println(sn.query(3));  // 4
+        out.println(sn.query(5));  // 2
+        sn = new SnakeEating(new Integer[]{15}, 3);
+        out.println(sn.query(15));  // 1
+        out.println(sn.query(16));  // 0
+        out.println(sn.query(1));   // 1
+        sn = new SnakeEating(new Integer[]{10, 15}, 3);
+        out.println(sn.query(15));  // 1
+        out.println(sn.query(16));  // 1
+        out.println(sn.query(10));  // 2
+        out.println(sn.query(17));  // 0
+        */
+        sn = new SnakeEating(new Integer[]{1000000000, 1500000000}, 3);
+        out.println(sn.query(1000000000));  // 2
+        out.println(sn.query(1500000000));  // 1
+        out.println(sn.query(1500000001));  // 1
+        out.println(sn.query(1500000002));  // 0
+        out.println(sn.query(1500000003));  // 0
+    }
+    static int[] sortIaR(int a[])  // sort int array reverse
+    {
+        return IntStream.of(a).boxed()
+                .sorted(Comparator.reverseOrder())
+                .mapToInt(i->i).toArray();        
+    }
+    static Integer[] ria(int N) { // read int array
+        Integer L[]=new Integer[N];
+        for (int i=0; i<N; i++)
+            L[i]=sc.nextInt();
+        return L;
+    }
+    
+    static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args)
+    {      
+        test();
+        /*int T=sc.nextInt(); // 1 ≤ T ≤ 5
+        for (int i=0; i<T; i++) {
+            int N=sc.nextInt(); // 1 ≤ N, Q ≤ 10^5
+            int Q=sc.nextInt();
+            Integer L[]=ria(N);     // 1 ≤ Li ≤ 10^9
+            new SnakeEating(L, Q).query();
+        }*/
+    }
+}
