71e7d01315d73c8e38af1cde137503de23644129
ANDY-PROBOOK\Will <willwsxu@gmail.com>
2017-05-24 21:39:04 -0500

    snake eating editorial version still WA

--- src/SnakeEating.java
+++ src/SnakeEating.java
@@ -26,20 +26,23 @@ class SnakeEating {
         //out.println(Arrays.toString(L));
         //out.println(Arrays.toString(prefix));
     }
-    int maxK=100000;
+    int maxK=1000000000;
     // Editorial idea
     SnakeEating(Integer a[], int q, boolean ascending)
     {
-        L=new Integer[a.length+1];
+        int n=a.length;
+        L=new Integer[n+1];
         Arrays.sort(a);
         Q=q;
-        prefix = new long[L.length+1];
+        prefix = new long[n+1];
         prefix[0]=0;
         L[0]=0;
-        for (int i=0; i<L.length; i++) {
+        for (int i=0; i<n; i++) {
             L[i+1]=a[i];
             prefix[i+1]=prefix[i]+maxK-a[i];
         }
+        //out.println(Arrays.toString(L));
+        //out.println(Arrays.toString(prefix));
     }
     long diff(int start, int next, int k) {
         long diff=prefix[next+1];
@@ -105,12 +108,15 @@ class SnakeEating {
     }
     
     /*
-        0 1  2  3  4  5   N=5, K=5
+        0 1  2  3  4  5   N=5
         0 14 13 12 11 10
         0 14 27 39 50 50 prefix
+    K=5, p3=4
+    k=6
     */
     int binaryEatSnakeAsc(int p1, int p2, int p3, int k)
     {
+        //out.println("p1="+p1+" p2="+p2);
         if (p1>=p2)
             return p1;
         int mid = (p1+p2)/2;
@@ -118,7 +124,7 @@ class SnakeEating {
         if (eat>mid)
             return binaryEatSnakeAsc(mid+1, p2, p3, k);
         else 
-            return binaryEatSnakeAsc(p1, mid-1, p3, k);
+            return binaryEatSnakeAsc(p1, mid, p3, k);
     }
     // keep track index of snakes to be eaten(p1), and no less than k (p3)
     int queryAsc(int k)  // sorted ascending
@@ -130,8 +136,8 @@ class SnakeEating {
         if ( p3<=2)
             return L.length-p3;
         p3--;
-        int p1= binaryEatSnakeAsc(1, p3-1, p3, k);
-        out.println("p1="+p1+" p3="+p3);
+        int p1= binaryEatSnakeAsc(1, p3, p3, k);
+        //out.println("p1="+p1+" p3="+p3+" k="+k);
         return L.length-p1-1;
     }
     void query()
@@ -145,6 +151,13 @@ class SnakeEating {
             out.println(bruteforce(k));
         }
         //out.print(sb.toString());
+    }
+    void queryAsc()
+    {
+        for (int i=0; i<Q; i++) {
+            int k=sc.nextInt(); // 1 ≤ Ki ≤ 10^9
+            out.println(queryAsc(k));
+        }
     }
     int bruteforce(int k)
     {
@@ -170,11 +183,12 @@ class SnakeEating {
             large[i]=rnd.nextInt(v)+1;
         }
         SnakeEating sn = new SnakeEating(large, 2);
+        SnakeEating sn2 = new SnakeEating(large, 2, true);
         for (int i=0; i<100000; i++)
         {
             int k=rnd.nextInt(v)+1;
             int a1=sn.query(k);
-            int a2=sn.queryAsc(k);
+            int a2=sn2.queryAsc(k);
             if (a1 !=a2) {
                 out.println("not equal k="+ k+" a1="+a1+" a2="+a2);
                 out.println(Arrays.toString(large));
@@ -182,6 +196,27 @@ class SnakeEating {
             }
         }       
     }
+    
+    static void test2()
+    {
+        SnakeEating sn = new SnakeEating(new Integer[]{1, 2, 3, 4, 5}, 2, true);
+        /*out.println(sn.queryAsc(100)==0);
+        out.println(sn.queryAsc(8)==1);
+        out.println(sn.queryAsc(6)==2);
+        out.println(sn.queryAsc(5)==2);
+        out.println(sn.queryAsc(3)==4);
+        out.println(sn.queryAsc(2)==4);
+        out.println(sn.queryAsc(1)==5);*/
+        Integer[] a100=new Integer[]{
+            23432544, 30094068, 30464197, 57517264, 72824160, 83379129, 84369085, 90217336, 93722419, 98007261, 115853901, 135627648, 142611359, 150220998, 153411949, 153452023, 157367463, 158646919, 161517301, 162892397, 170786068, 172376911, 190345745, 199684026, 219931970, 234509573, 245025337, 273118574, 277344220, 277991209, 295773933, 303545198, 304236008, 306196106, 309731545, 314152979, 341796016, 354850621, 366387332, 397545640, 398435503, 406112336, 414517477, 419553630, 440122384, 448743018, 461495568, 468053082, 474701816, 477145799, 479537600, 485472186, 500958007, 506053693, 512458375, 515301908, 546713157, 549549805, 557400352, 572097736, 590964007, 608242210, 613751584, 616455157, 622831894, 630802360, 634820806, 635951454, 643541624, 645301000, 650945405, 652635438, 665666350, 671957934, 675823675, 685958625, 689349673, 701197451, 736013095, 776054792, 784535868, 788219822, 794817320, 802068984, 802676504, 836677218, 872692418, 879161324, 882057533, 885679738, 887621415, 889808366, 901777887, 908835105, 912971816, 914098635, 922131840, 947166781, 974291786, 976528044
+        };
+        sn = new SnakeEating(a100, 2, true);
+        out.println(sn.queryAsc(342363793));
+        SnakeEating sn1 = new SnakeEating(a100, 2);
+        out.println(sn1.bruteforce(342363793));
+        out.println(sn1.query(342363793));
+    }
+    
     static void test()
     {
         SnakeEating sn = new SnakeEating(new Integer[]{21, 9, 5, 8, 10}, 2);
@@ -269,14 +304,14 @@ class SnakeEating {
     static Scanner sc = new Scanner(System.in);
     public static void main(String[] args)
     {      
-        autotest();
-        /*int T=sc.nextInt(); // 1 ≤ T ≤ 5
+        //test2();
+        int T=sc.nextInt(); // 1 ≤ T ≤ 5
         for (int i=0; i<T; i++) {
             int N=sc.nextInt(); // 1 ≤ N, Q ≤ 10^5
             int Q=sc.nextInt();
             Integer L[]=ria(N);     // 1 ≤ Li ≤ 10^9
-            new SnakeEating(L, Q).query();
-        }*/
+            new SnakeEating(L, Q, true).queryAsc();
+        }
     }
 }
 /*
