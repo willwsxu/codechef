be1f1901df5fd3f20eed0cfadbe74c426b2286c2
willwsxu <willwsxu@gmail.com>
2017-06-02 17:25:05 -0500

    snake graph DSU basic code

--- src/smackdown/roundb/SnakeGraph.java
+++ src/smackdown/roundb/SnakeGraph.java
@@ -1,8 +1,11 @@
 package smackdown.roundb;
 
 
+import java.util.AbstractMap.SimpleEntry;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.PriorityQueue;
 import java.util.Scanner;
 
 interface IGraphX
@@ -40,21 +43,30 @@ class SimpleGraphX implements IGraphX { // unweighted, bidirectional
     }
 }
 
-class CCx
+// only process edge if both nodes has degree>d
+class DSU
 {
     private IGraphX g;
     private int visId[];  // dual purpose, for component id and is visited
     private int id=0;
-    public CCx(IGraphX g)
+    PriorityQueue<Map.Entry<Integer, Integer>> degree = new PriorityQueue<>(100000, (e1,e2)->e2.getValue()-e1.getValue());
+    public DSU(IGraphX g)
     {
         this.g=g;
         visId = new int[g.V()];
         for (int s = 0; s < g.V(); s++)
             // no need to visit nodes without any edge
-            if (visId[s]==0 && !g.adj(s).isEmpty())
+            if (!g.adj(s).isEmpty())
             {
-                dfs(s, ++id);
+                degree.add(new SimpleEntry(s,g.adj(s).size()));
             }
+    }
+    public void dfs(int d)
+    {
+        for (Map.Entry<Integer, Integer> e:degree) {
+            if (e.getValue()>d && visId[e.getKey()]==0)
+                dfs(e.getKey(), ++id);
+        }
     }
     private void dfs(int v, int id) {
         //out.println("dfs v"+v+" id="+id);
@@ -89,9 +101,20 @@ class CCx
 
 // SNGraph, Medium, DSU (Disjoint Set Union)
 class SnakeGraph {
+    SnakeGraph()
+    {
+        int n=sc.nextInt(); // 1 ≤ n ≤ 10^5
+        int m=sc.nextInt();//0 ≤ m ≤ min(n * (n - 1) / 2, 2 * 10^5)
+        SimpleGraphX g=new SimpleGraphX(n);
+        for (int j=0; j<m; j++) {
+            int u=sc.nextInt();
+            int v=sc.nextInt();
+            g.addEdge(u-1, v-1);
+        }
+    }
     static void solve(SimpleGraphX g)
     {
-        CCx cc=new CCx(g);
+        DSU cc=new DSU(g);
         StringBuilder sb = new StringBuilder();
         sb.append(cc.numCompoments()-1);
         for (int i=1; i<g.V(); i++) {
@@ -99,24 +122,15 @@ class SnakeGraph {
         }        
     }
    
-    static void autotest()
+    static void manualtest()
     {
-        int T=sc.nextInt();     // 1 ≤ T ≤ 3
-        for (int i=0; i<T; i++) {
-            int n=sc.nextInt(); // 1 ≤ n ≤ 10^5
-            int m=sc.nextInt();//0 ≤ m ≤ min(n * (n - 1) / 2, 2 * 10^5)
-            SimpleGraphX g=new SimpleGraphX(n);
-            for (int j=0; j<m; j++) {
-                int u=sc.nextInt();
-                int v=sc.nextInt();
-                g.addEdge(u-1, v-1);
-            }
-        }        
     }
     
     static Scanner sc = new Scanner(System.in);
     public static void main(String[] args)
     {      
-        autotest();
+        int T=sc.nextInt();     // 1 ≤ T ≤ 3
+        while (T-->0)
+            new SnakeGraph();
     } 
 }
