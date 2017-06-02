5781d1d7a5668ee78b588b22359f9d6106656afb
willwsxu <willwsxu@gmail.com>
2017-06-02 09:39:07 -0500

    snake social BFS impl

--- src/smackdown/roundb/SnakeSocial.java
+++ src/smackdown/roundb/SnakeSocial.java
@@ -1,5 +1,7 @@
 package smackdown.roundb;
 
+
+
 /*
  * Brief Desc: snake houses in a n x m grid
  * A bill has been passed for making a smooth transition from capitalism to socialism. 
@@ -19,20 +21,33 @@ package smackdown.roundb;
  * will be the answer. 
  * Create a artificial node to connect to max nodes, so it becomes single source BFS
 */
+import static java.lang.Integer.max;
 import static java.lang.System.out;
 import java.util.AbstractMap.SimpleEntry;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
 // SNSOCIAL, Easy - Medium, BFS
 class SnakeSocial {
     int a[][];
     int maxV;
-    SnakeSocial(int a[][], int m)
+    static final int x[]=new int[]{-1,-1,-1,0,0,1,1,1};
+    static final int y[]=new int[]{-1,0,1,-1,1,-1,0,1};
+    List<Map.Entry<Integer, Integer>> maxEntry=new ArrayList<>();
+    
+    SnakeSocial()
     {
-        List<Map.Entry<Integer, Integer>> maxEntry=new ArrayList<>();
+        int n=sc.nextInt();     // 1 ≤ n, m ≤ 50
+        int m=sc.nextInt();
+        int a[][]=new int[n][m];// 1 ≤ a[i][j] ≤ 10^6
+        int mx=fillMatrix(a, sc);  
+        init(a,mx);
+    }
+    private void init(int a[][], int m)
+    {
         for (int i=0; i<a.length; i++)
             for (int j=0; j<a[i].length; j++) {
                 if ( a[i][j]==m)
@@ -40,11 +55,15 @@ class SnakeSocial {
             }
         this.a=a;
         maxV=m;
-        out.println(adhoc(maxEntry));
+    }
+    boolean valid(int i, int j) {        
+        if (i<0 || j<0 || i>=a.length || j>=a[0].length)
+            return false;
+        return true;
     }
     void update(int i, int j, List<Map.Entry<Integer, Integer>> newEntry)
     {
-        if (i<0 || j<0 || i>=a.length || j>=a[0].length)
+        if ( !valid(i,j) )
             return;
         if(a[i][j]>=maxV)
             return;
@@ -57,8 +76,6 @@ class SnakeSocial {
         for (Map.Entry<Integer, Integer> e: entry) {
             int i=e.getKey();
             int j=e.getValue();
-            int x[]=new int[]{-1,-1,-1,0,0,1,1,1};
-            int y[]=new int[]{-1,0,1,-1,1,-1,0,1};
             for (int k=0; k<x.length; k++)
                 update(i+x[k], j+y[k], newEntry);
         }
@@ -67,6 +84,41 @@ class SnakeSocial {
         else {
             return 1+adhoc(newEntry);
         }
+    }
+    boolean vis[][];
+    int d[][];
+    int bfs()
+    {
+        int n=a.length;
+        int m=a[0].length;
+        vis = new boolean[n][m];
+        d=new int[n][m];
+        ConcurrentLinkedQueue<Map.Entry<Integer, Integer>> q=new ConcurrentLinkedQueue<>();
+        for (Map.Entry<Integer, Integer> e: maxEntry) {
+            q.add(e);
+            vis[e.getKey()][e.getValue()]=true;
+        }
+        int ans=0;
+        while (!q.isEmpty()) {
+            Map.Entry<Integer, Integer> e=q.poll();
+            ans=max(ans, d[e.getKey()][e.getValue()]);
+            for (int k=0; k<x.length; k++) {
+                int i=e.getKey()+x[k];
+                int j=e.getValue()+y[k];
+                if ( valid(i,j) && !vis[i][j] ) {
+                    q.add(new SimpleEntry<Integer,Integer>(i,j) );
+                    d[i][j] = d[e.getKey()][e.getValue()]+1;
+                    vis[i][j]=true;
+                }                    
+            }
+        }
+        return ans;
+    }
+    
+    void solve()
+    {
+        out.println(bfs());    
+        //out.println(adhoc(maxEntry));        
     }
     static int fillMatrix(int [][] a, Scanner reader) // shared
     {
@@ -83,12 +135,7 @@ class SnakeSocial {
     public static void main(String[] args)
     {      
         int T=sc.nextInt();     // 1 ≤ T ≤ 4
-        for (int i=0; i<T; i++) {
-            int n=sc.nextInt();     // 1 ≤ n, m ≤ 50
-            int m=sc.nextInt();
-            int a[][]=new int[n][m];// 1 ≤ a[i][j] ≤ 10^6
-            int mx=fillMatrix(a, sc);
-            new SnakeSocial(a, mx);
-        }
+        while (T-->0)
+            new SnakeSocial().solve();
     }
 }
