9d2a8e618381810c24d5f1b701f20fd5ec6b61c6
ANDY-PROBOOK\Will <willwsxu@gmail.com>
2017-06-04 21:45:15 -0500

    posttree refactoring, dfs

--- src/LunchTime/may2017/PostTree.java
+++ src/LunchTime/may2017/PostTree.java
@@ -3,37 +3,101 @@ package LunchTime.may2017;
 
 import static java.lang.Long.min;
 import static java.lang.System.out;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 /*
-
+ * Given a tree with each node having some value. You need to find the cost of 
+ * path from each node v to the root of the tree. Cost of each node u in path is 
+ * the minimum of value of each node lying below u and is in path from (u, v).
  */
 
 // POSTTREE, EASY (more like MEDIUM)
+// Binary Lifting on a tree, lowest common ancestor, DFS
+// https://discuss.codechef.com/questions/99371/posttree-editorial
 public class PostTree {
     
-    long c[];
+    long cn[];// cost of node
+    long cp[];// cost of path
     int parent[];
-    long w[];
-    PostTree(int parent[], long w[])
+    int w[];
+    PostTree(int parent[], int w[])
     {
-        c=new long[w.length];
-        for (int i=1; i<w.length; i++)
-            c[i]=w[i];
         this.parent=parent;
         this.w=w;
         cost(w.length-1);
-        for (int i=1; i<w.length;i++)
-            out.print(c[i]+" ");
-        out.println();
+        init();
+    }
+    PostTree()
+    {
+        int N=sc.nextInt(); // 1 ≤ N ≤ 100,000
+        parent=new int[N];
+        parent[0]=0;
+        for (int i=1; i<N; i++) { 
+            parent[i]=sc.nextInt()-1;
+        }
+        w=ria(N, sc); //-1,000,000,000 ≤ Av ≤ 1,000,000,000
+        init();
+    }
+    SimpleGraphX g;
+    private void init()
+    {
+        int N=w.length;
+        cn=new long[N];
+        for (int i=0; i<N; i++)
+            cn[i]=w[i];    
+        g = new SimpleGraphX(N);
+        for (int i=1; i<N; i++) { 
+            g.addEdge(i, parent[i]);
+        }
+        cp=new long[N];
+        cp[0]=cn[0];
+        vis=new boolean[N];
+        dfs(0);
+        print(cn);
+        //Arrays.fill(vis, false);
+        //dfs2(0);
+        print(cp);
+    }
+    void print(long x[])
+    {
+        for (int i=0; i<x.length;i++)
+            out.print(x[i]+" ");
+        out.println();        
+    }
+    boolean vis[];
+    void dfs(int v) {
+        vis[v]=true;
+        if ( v>0) {
+            int p=parent[v];
+            cp[v] = cn[v]+cp[p] ;
+            out.println((v+1)+"="+cp[v]);
+        }
+        for (int w: g.adj(v))
+            if (!vis[w])
+                dfs(w);
+        int p=parent[v];
+        cn[p] = min(cn[p], cn[v]);
+    }
+    void dfs2(int v) {
+        vis[v]=true;
+        if ( v>0) {
+            int p=parent[v];
+            cp[v] = cn[v]+cp[p] ;
+            out.println((v+1)+"="+cp[v]);
+        }
+        for (int w: g.adj(v))
+            if (!vis[w])
+                dfs2(w);
     }
     void update(int p)
     {
         while (p>1) {
             int g=parent[p];
-            if (c[g]>c[p])
-                c[g]=c[p];
+            if (cn[g]>cn[p])
+                cn[g]=cn[p];
             else
                 break;
         }
@@ -45,20 +109,18 @@ public class PostTree {
             cost(n-1);
         //out.println("n="+n+" c "+c[n]);
     }
+      
+    public static int[] ria(int N, Scanner sc) { // read int array
+        int L[]=new int[N];
+        for (int i=0; i<N; i++)
+            L[i]=sc.nextInt();
+        return L;
+    }
     
     static Scanner sc = new Scanner(System.in);
     public static void main(String[] args)
     {      
-        int N=sc.nextInt(); // 1 ≤ N ≤ 100,000
-        int parent[]=new int[N+1];
-        parent[1]=1;
-        for (int i=2; i<=N; i++) { 
-            parent[i]=sc.nextInt();
-        }
-        long w[]=new long[N+1];
-        for (int i=1; i<=N; i++)
-            w[i]=sc.nextLong();  //-1,000,000,000 ≤ Av ≤ 1,000,000,000
-        new PostTree(parent, w);
+        new PostTree();
     }
 }
 /*
@@ -66,3 +128,39 @@ public class PostTree {
 1 1 1 1 5 8 6
 1 2 3 4 5 15 70 10
 */
+
+interface IGraphX
+{
+    int V();
+    public List<Integer> adj(int u);
+}
+
+// vertex from 0
+class SimpleGraphX implements IGraphX { // unweighted, bidirectional
+    protected int   V; // number of vertices
+    private   int   E; // number of edges
+
+    private List<List<Integer>> adj;
+    public SimpleGraphX(int V)
+    {
+        adj=new ArrayList<>(V);
+        this.V = V;
+        E=0;
+        for (int v = 0; v < V; v++) // Initialize all lists
+            adj.add( new ArrayList<>(10));
+    }
+    public int V() { return V; }
+    public int E() { return E; }
+    
+    public void addEdge(int u, int v)
+    {
+        //out.println("add edge "+u+","+v);
+        adj.get(u).add(v);
+        adj.get(v).add(u);
+        E++;
+    }
+    public List<Integer> adj(int u)
+    {
+        return adj.get(u);
+    }
+}
