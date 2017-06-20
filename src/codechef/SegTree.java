d91200cd92b4f27d3bd32f42b8f8cbdffed1882e
ANDY-PROBOOK\Will <willwsxu@gmail.com>
2017-06-19 22:20:41 -0500

    SegTree sum refactoring

--- src/codechef/SegTree.java
+++ src/codechef/SegTree.java
@@ -6,23 +6,23 @@ package codechef;
 class SegTree
 {
     int sum[];
-   SegTree(int N) 
-   {
-       sum=new int[2*N+1];
-       buildSum(1, 0, N-1);
-   }
-   int buildSum(int node, int first, int last)
-   {
-       if (first==last)
-           sum[node]=compute(first);
-       else {
-           int mid = (first+last)/2;
-           sum[node]=buildSum(2*node, first, mid)+buildSum(2*node+1, mid+1, last);
-       }
-       return sum[node];
-   }
-   int compute(int ind)
-   {
-       return 0;
-   }
+    SegTree(int N) 
+    {
+        sum=new int[2*N+1];
+        buildSum(1, 0, N-1);
+    }
+    int buildSum(int node, int first, int last)
+    {
+        if (first==last)
+            sum[node]=compute(first);
+        else {
+            int mid = (first+last)/2;
+            sum[node]=buildSum(2*node, first, mid)+buildSum(2*node+1, mid+1, last);
+        }
+        return sum[node];
+    }
+    int compute(int ind)
+    {
+        return 0;
+    }
 }
