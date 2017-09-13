fa3222b0522cb4de632c034104cb833ae5e60d10
willwsxu <willwsxu@gmail.com>
2017-09-13 17:30:56 -0500

    commands basic code

--- src/SerejaCommands.java
+++ src/SerejaCommands.java
@@ -2,19 +2,24 @@
 class SerejaCommands {
     int cmdsFr[];  // 
     int cmdsTo[];
+    int cmdsType[];
     int cmdsCount[];
     
     void parse(int cmds[])
     {
         int m=cmds.length/3;
+        cmdsFr = new int[m];
+        cmdsTo = new int[m];
+        cmdsType = new int[m];
+        cmdsCount = new int[m];
         for (int i=0; i<m; i++) {
-            if (cmds[3*i]==1) {
-                cmdsFr[i]=cmds[3*i+1];
-                cmdsTo[i]=cmds[3*i+2];
-                cmdsCount[i]++;
-            } else {
-                
-            }
+            cmdsType[i]=cmds[3*i];
+            cmdsFr[i]=cmds[3*i+1];
+            cmdsTo[i]=cmds[3*i+2];
+        }
+        cmdsCount[m-1]++;
+        for (int i=m-1; i>=0; i--) {
+            
         }
     }
     
