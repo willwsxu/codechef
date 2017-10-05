3db9cf17d47c6e79cb6b81021cccb2c36ea4007f
willwsxu <willwsxu@gmail.com>
2017-10-05 17:28:20 -0500

    bank hacking idea

--- src/codeforces/r408/BankHacking796C.java
+++ src/codeforces/r408/BankHacking796C.java
@@ -12,8 +12,13 @@ import java.util.List;
 // Bank i and j are semi-neighboring if and only if there exists an ONLINE bank k such that i and k are neighboring and k and j are neighboring.
 // When a bank is hacked, it becomes offline, and other banks that are neighboring or semi-neighboring to it have their strengths increased by 1.
 // From above, we can conclude that each bank strength increase at most 2
-// Pick a bank to start, then repeat one that must be neighboring to some offline bank and strength<=hacker
+// Pick a bank to start, then repeat, each must be neighboring to some offline bank and strength<=hacker
 // find the minimum strength of hacker's computer strength
+// IDEA: find m=max(bank 1 to n), answer can only be one of m, m+1, or m+2
+//       count # of banks with strength m, or m-1. other banks does impact answer
+// Iterate each bank, find its neighbours with strength m or m-1. find if there are other banks with strength m or m-1 (use simple math as total m or m-1 is known).
+//       if other bank has m, then ans=m+2, else if other bank has m-1, then ans=m+1, else if neighbor has m, ans=m+1, else ans=m
+// final answer is min (ans from 1 to n)
 public class BankHacking796C {
     
     int strength[];         //- 10^9 ≤ ai ≤ 10^9, 1 ≤ n ≤ 3·10^5
