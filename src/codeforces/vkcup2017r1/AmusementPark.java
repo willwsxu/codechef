49987cd8f3791ce7aeb6d9d3b504a71a46d9776e
ANDY-PROBOOK\Will <willwsxu@gmail.com>
2017-10-07 14:05:43 -0500

    amusement park, check answer if it is best, pass all tests

--- src/codeforces/vkcup2017r1/AmusementPark.java
+++ src/codeforces/vkcup2017r1/AmusementPark.java
@@ -1,13 +1,16 @@
 package codeforces.vkcup2017r1;
 
 
+import static java.lang.Math.abs;
 import static java.lang.System.out;
 import java.util.Scanner;
 
 // 795A
 // n people, some pupil, some adult. separate into groups. a group must have 1 adult
 // if a group has x people, cost of tickets=c1+c2(x-1)^2
-// find minimal price
+// optimize grouping to find minimal price
+// IDEA: binary search, local minimum may not be the best answer as we may have picked the wrong half
+//  need to verify if there is no better solution
 public class AmusementPark {
     
     int adult;
@@ -30,16 +33,28 @@ public class AmusementPark {
         int avg = n/group;
         int rem = n%group;
         long cost = formula(avg);
-        if ( rem>0 ) {
-            cost *= (group-rem);
-            return cost + rem*formula(avg+1);
-        }
-        else 
-            return cost *group;
+        cost *= (group-rem);
+        return cost + rem*formula(avg+1);
     }
     long cost(int lo, int hi, long lowCost, long hiCost)
     {
-        if (lo-hi<=1 && lo-hi>=-1) {
+        if (abs(lo-hi)<=1) {
+            //out.println("first "+lo+" second "+hi+" low clost "+lowCost+" hi cost "+hiCost);
+            
+            int temp=lo;
+            while (temp>1) {
+                long c=calc(--temp);
+                if (c>lowCost)
+                    break;
+                lowCost=c;
+            }
+            temp=lo;
+            while (temp<adult) {
+                long c=calc(++temp);
+                if (c>lowCost)
+                    break;
+                lowCost=c;
+            }
             return lowCost;
         }
         int mid = (lo+hi)/2;
@@ -47,7 +62,7 @@ public class AmusementPark {
         //out.println("first "+lo+" second "+hi+ " mid cost "+midCost+" low clost "+lowCost+" hi cost "+hiCost);
         if (midCost<lowCost)
             return cost(mid, lo, midCost, lowCost);
-        else if ( midCost < hiCost )
+        else if ( midCost > lowCost )
             return cost(lo, mid, lowCost, midCost);
         else {
             //out.println("bad mid cost "+midCost+" low clost "+lowCost+" hi cost "+hiCost);
@@ -64,15 +79,16 @@ public class AmusementPark {
         long cost1 = calc(1);
         long cost2 = calc(a);
         if (cost1>cost2)
-            out.println(cost(a, 1, cost2, cost1));
+            cost1=cost(a, 1, cost2, cost1);
         else if (cost1<cost2)
-            out.println(cost(1, a, cost1, cost2));
-        else
-            out.println(cost1);
+            cost1=cost(1, a, cost1, cost2);
+        //if (cost1==9343400)
+        //    out.println("adult="+a+" pupil="+p+" cost1="+calc(1)+" cost2="+cost2);
+        out.println(cost1);
     }
     
     static Scanner scan = new Scanner(System.in);
-    static void autoTest()
+    static void judge()
     {        
         int n = scan.nextInt();  // between 1 and 20000
         int c1 = scan.nextInt();  // between 1 and 10^7
@@ -91,9 +107,14 @@ public class AmusementPark {
         }
         new AmusementPark(adult, pupil, c1, c2);
     }
+    static void test()
+    {
+        new AmusementPark(9559, 10442, 1000, 100); // 9333800
+        new AmusementPark(9, 1, 2, 2); // 20
+    }
     public static void main(String[] args)
     {
-        autoTest();
+        judge();
     }
 }
 /*
