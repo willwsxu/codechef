dac2d8297e25256af6395191cc305ff67321ca00
ANDY-PROBOOK\Will <willwsxu@gmail.com>
2017-10-23 20:57:55 -0500

    sub inverse notes

--- src/SubInversing.java
+++ src/SubInversing.java
@@ -1,9 +1,11 @@
 
 import java.util.Scanner;
 
-/*
+/* String initially all 0 (size N). q queries to inverse bits on subsegment [L,R]
+ * from 0 to 1, or 1 to 0. output the lexicographically maximal sequence among all you got.
  */
-
+// SUBIINVER, medium, lunch time May 2017
+// persistent segment tree and hashes OR bitsets
 public class SubInversing {
    
     static Scanner sc = new Scanner(System.in);
