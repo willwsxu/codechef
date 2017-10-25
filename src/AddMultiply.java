adcf054f48feff884b53135274c5e2797ec3d8c2
willwsxu <willwsxu@gmail.com>
2017-10-25 16:08:21 -0500

    add multiply fix MOD, TLE

--- src/AddMultiply.java
+++ src/AddMultiply.java
@@ -3,8 +3,26 @@
  * Find sum of result of all possible expressions
 */
 import static java.lang.System.out;
+import java.util.Arrays;
 import java.util.Scanner;
 
+class Power2
+{
+    static final int  MOD=1000000007;    // 10^9 + 7
+
+    private static int power2[]=new int[300000+5];
+    static
+    {
+        power2[0]=1;
+        for (int i=1; i< power2.length; i++) {
+            power2[i] = (2 * power2[i-1])%MOD;
+        }
+        //out.println(Arrays.toString(power2));
+    }
+    public static int get(int i) {
+        return power2[i];
+    }
+}
 // PLUSMUL, Smackdown 2017 online elimination round. dp
 // https://discuss.codechef.com/questions/100802/how-to-solve-the-question-plusmul-from-snackdown-elimination-round
 class AddMultiply {
@@ -45,7 +63,7 @@ class AddMultiply {
     // i=1, inner: j=1, m2=1, add up 2 2s; j=2, add up 2X3;         j=3, add up 2x3x4
     // i=2, inner: j=2, m1=1, add up 2 3s (one at top half, one at bottom half);    j=3, add 2 3x4
     // i=3, inner: j=3, m1=2, add up 4 4s (one from each quater)
-    void solve()
+    long solve()
     {
         long ans=0;
         for (int i=0; i<a.length; i++) {
@@ -56,20 +74,21 @@ class AddMultiply {
                 int m2=n-2-j;
                 if (m2<0)
                     m2=0;
-                long ans1 = (prod * (1<<m2) * (1<<m1))%MOD;
-                out.println("j="+j+" p="+prod+" a "+ans1);
+                long ans1 = (prod * Power2.get(m2))%MOD;
+                ans1 = (ans1 * Power2.get(m1))%MOD;
+                //out.println("j="+j+" p="+prod+" a "+ans1);
                 ans = (ans+ans1)%MOD;
             }
         }
-        out.println(ans);
+        return ans;
     }
     
     static void test()
     {
-        new AddMultiply(new int[]{1,2,4}).solve();
-        new AddMultiply(new int[]{1,1,1,1}).solve();
-        new AddMultiply(new int[]{2,3,5,7}).solve();
-        new AddMultiply(new int[]{200000000,300000000,500000000,700000000}).solve();
+        out.println(new AddMultiply(new int[]{1,2,4}).solve()==30);
+        out.println(new AddMultiply(new int[]{1,1,1,1}).solve()==20);
+        out.println(new AddMultiply(new int[]{2,3,5,7}).solve()==494);
+        out.println(new AddMultiply(new int[]{200000000,300000000,500000000,700000000}).solve()==822000021);
     }
       
     public static int[] ria(int N, Scanner sc) { // read int array
@@ -81,12 +100,12 @@ class AddMultiply {
     static Scanner sc = new Scanner(System.in);
     public static void main(String[] args)
     {      
-        test();
+        judge();
     }
     static void judge()
     {
         int T=sc.nextInt(); // 1 ≤ T ≤ 500
         while (T-->0)
-            new AddMultiply().solve();        
+            out.println(new AddMultiply().solve());        
     }
 }
