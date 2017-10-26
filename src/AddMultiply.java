91c937eca72d290ffe3fa8ee7a6129b8321ac95b
ANDY-PROBOOK\Will <willwsxu@gmail.com>
2017-10-25 21:25:09 -0500

    add multiply, run time error

--- src/AddMultiply.java
+++ src/AddMultiply.java
@@ -24,43 +24,59 @@ class Power2
     }
 }
 // PLUSMUL, Smackdown 2017 online elimination round. dp
+// Use Thread class to provide a large call stack for recursion
 // https://discuss.codechef.com/questions/100802/how-to-solve-the-question-plusmul-from-snackdown-elimination-round
-class AddMultiply {
+class AddMultiply implements Runnable {
     int a[];
     static final int MOD=1000000007;
     int n;
-    long dp[];
-    AddMultiply()
+    long dp_f[];
+    long dp_g[];
+    long dp_h[];
+    AddMultiply(int n)
     {
-        n=sc.nextInt(); // 1 ≤ n ≤ 100000
-        a=ria(n, sc);
+        this(ria(n, sc));
     }
     AddMultiply(int a[])
     {
         this.a=a;
         n=a.length;
+        dp_f=new long[n];
+        dp_g=new long[n];
+        dp_h=new long[n];
+        Arrays.fill(dp_f, -1);
+        Arrays.fill(dp_g, -1);
+        Arrays.fill(dp_h, -1);
+        dp_f[0]=a[0];
+        dp_g[0]=a[0];
+        dp_h[0]=a[0];
     }
     
     // f(i)=g(i-1)+h(i)
     long f(int i)
     {
-        if (i==0)
-            return a[i];
-        return g(i-1)+h(i);
+        if (dp_f[i]>=0)
+            return dp_f[i];
+        dp_f[i]=(g(i-1)+h(i))%MOD;
+        return dp_f[i];
     }
     // g(i)=g(i-1)+f(i)
     long g(int i)
     {
-        if (i==0)
-            return a[i];
-        return g(i-1)+f(i);
+        if (dp_g[i]>=0)
+            return dp_g[i];
+        dp_g[i]=(g(i-1)+f(i))%MOD;
+        return dp_g[i];
     }
     // h(i)=h(i-1)xai + (2^i-1)ai
     long h(int i)
     {
-        if (i==0)
-            return a[i];
-        return h(i-1)*a[i]+Power2.get(i-1)*a[i];
+        if (dp_h[i]>=0)
+            return dp_h[i];
+        long v1=h(i-1)*a[i];
+        long v2=(long)Power2.get(i-1)*a[i];  // watch out for overflow of int *int
+        dp_h[i] = (v1%MOD+v2%MOD)%MOD;
+        return dp_h[i];
     }
     
     // pattern 
@@ -78,7 +94,7 @@ class AddMultiply {
     // i=1, inner: j=1, m2=1, add up 2 2s; j=2, add up 2x3;         j=3, add up 2x3x4
     // i=2, inner: j=2, m1=1, add up 2 3s (one at top half, one at bottom half);    j=3, add 2 3x4
     // i=3, inner: j=3, m1=2, add up 4 4s (one from each quarter)
-    long solve()
+    long solve()  //O(n^2) TLE
     {
         long ans=0;
         for (int i=0; i<a.length; i++) {
@@ -97,6 +113,14 @@ class AddMultiply {
         }
         return ans;
     }
+    long dp()
+    {
+        return f(n-1);
+    }
+    public void run()
+    {
+        out.println(f(n-1));
+    }
     
     static void test()
     {
@@ -104,6 +128,10 @@ class AddMultiply {
         out.println(new AddMultiply(new int[]{1,1,1,1}).solve()==20);
         out.println(new AddMultiply(new int[]{2,3,5,7}).solve()==494);
         out.println(new AddMultiply(new int[]{200000000,300000000,500000000,700000000}).solve()==822000021);
+        out.println(new AddMultiply(new int[]{1,2,4}).dp()==30);
+        out.println(new AddMultiply(new int[]{1,1,1,1}).dp()==20);
+        out.println(new AddMultiply(new int[]{2,3,5,7}).dp()==494);
+        out.println(new AddMultiply(new int[]{200000000,300000000,500000000,700000000}).dp());
     }
       
     public static int[] ria(int N, Scanner sc) { // read int array
@@ -112,6 +140,7 @@ class AddMultiply {
             L[i]=sc.nextInt();
         return L;
     }
+    
     static Scanner sc = new Scanner(System.in);
     public static void main(String[] args)
     {      
@@ -120,7 +149,17 @@ class AddMultiply {
     static void judge()
     {
         int T=sc.nextInt(); // 1 ≤ T ≤ 500
-        while (T-->0)
-            out.println(new AddMultiply().solve());        
+        while (T-->0) {
+            int n=sc.nextInt(); // 1 ≤ n ≤ 100000
+            //out.println(new AddMultiply(n).dp());  
+            Thread t =new Thread(null, new AddMultiply(n), "whatever", 1<<28);
+            t.start();
+            try {
+                t.join();
+            } catch (Exception e)
+            {
+                
+            }
+        }
     }
 }
