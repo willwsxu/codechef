1257d27796840a487897df1e3d9652ca69ca80e4
willwsxu <willwsxu@gmail.com>
2017-11-14 12:39:58 -0600

    fingerprint refactor, another implementation

--- src/acsl/c1/Fingerprint.java
+++ src/acsl/c1/Fingerprint.java
@@ -9,10 +9,10 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Fingerprint {
-    static final int[] whorlNo=new int[]{1,1,2,2,4,16,16,8,8,4};
-    static final int[] fingerNo=new int[]{10,9,8,7,6,1,2,3,4,5};
+    static final int[] whorlNo=new int[]{  1,1,2,2,4,16,16,8,8,4};
+    static final int[] fingerNo=new int[]{10,9,8,7,6,1, 2, 3,4,5};
     static final int[] bitEven=new int[]{10, 8, 6, 4, 2};
-    static final int[] bitOdd=new int[]{9, 7, 5, 3, 1};
+    static final int[] bitOdd=new int[]{  9, 7, 5, 3, 1};
     
     
     void printList(List<Integer> ans)
@@ -28,33 +28,75 @@ public class Fingerprint {
          }   
          out.println();
     }
+    void decode(List<Integer> ans, int num, int[]bitmap)
+    {
+        for (int i=0; i<5; i++) {
+            if ((num & 1<<i)>0)
+                ans.add(bitmap[i]);
+        }        
+    }
+    
+    int getFinger(int whorl, boolean even) {
+        switch (whorl) {
+            case 16:
+                return even?2:1;
+            case 8:
+                return even?4:3;
+            case 4:
+                return even?6:5;
+            case 2:
+                return even?8:7;
+            case 1:
+                return even?10:9;
+        }
+        return 0;
+    }
+    // helper function to reduce code duplication
+    int addFinger(List<Integer> ans, int num, int whorl, boolean even)
+    {
+        ans.add(getFinger(whorl, even));
+        return num - whorl;        
+    }
+    void brute(List<Integer> ans, int num, boolean even)
+    {
+        while (num>0) {
+            if (num>=16) {
+                num = addFinger(ans, num, 16, even);
+            } else if (num>=8) {
+                num = addFinger(ans, num, 8, even);
+            } else if (num>=4) {
+                num = addFinger(ans, num, 4, even);
+            } else if (num>=2) {
+                num = addFinger(ans, num, 2, even);
+            } else if (num>=1) {
+                num = addFinger(ans, num, 1, even); 
+            }
+        }
+    }
     Fingerprint(int num, int denom)
     {
         List<Integer> ans=new ArrayList<>();
-        for (int i=0; i<5; i++) {
-            if ((num & 1<<i)>0)
-                ans.add(bitEven[i]);
-        }
-        for (int i=0; i<5; i++) {
-            if ((denom & 1<<i)>0)
-                ans.add(bitOdd[i]);
-        }
+        //decode(ans, num, bitEven);
+        //decode(ans, denom, bitOdd);
+        brute(ans, num, true);
+        brute(ans, denom, false);
         printList(ans);
     }    
     public static void test()
     {
-        new Fingerprint(19-1,3-1);
-        new Fingerprint(0,0);
-        new Fingerprint(1,0);
-        new Fingerprint(0,1);
-        new Fingerprint(24,12);
-        new Fingerprint(20,26);
+        new Fingerprint(19-1,3-1);  // 8, 2, 7
+        new Fingerprint(0,0);       // None
+        new Fingerprint(1,0);       // 10
+        new Fingerprint(0,1);       // 9
+        new Fingerprint(24,12);     // 4,2,5,3
+        new Fingerprint(20,26);     // 6,2,7,3,1
+        new Fingerprint(16,8);      // 2,3
+        new Fingerprint(8,4);       // 4,5
     }
     static Scanner sc=new Scanner(System.in).useDelimiter("\\s*[,\n]\\s*");
     public static void main(String[] args)
     {  
-        int num=sc.nextInt();
-        int denom=sc.nextInt();
-        new Fingerprint(num-1, denom-1);
+        //test();
+        new Fingerprint(sc.nextInt()-1, sc.nextInt()-1);
     }
 }
