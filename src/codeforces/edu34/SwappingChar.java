4acf4eb5570a421ac454221c3457ea894a27cb80
ANDY-PROBOOK\Will <willwsxu@gmail.com>
2017-12-16 23:39:41 -0600

    swapping char, pass one test

--- src/codeforces/edu34/SwappingChar.java
+++ src/codeforces/edu34/SwappingChar.java
@@ -7,6 +7,7 @@ package codeforces.edu34;
 
 import static java.lang.System.out;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.TreeSet;
@@ -48,6 +49,39 @@ public class SwappingChar {
                     return "-3";
             }
         }
+        // find position of diff char of first 2 string
+        ArrayList<Integer> diff1=new ArrayList<>();
+        for (int j=0; j<u[0].length(); j++) {
+            if (u[0].charAt(j) !=u[1].charAt(j))
+                diff1.add(j);
+        }
+        TreeSet<String> candidates=new TreeSet<>();
+        for (int k=0; k<diff1.size()-1; k++) { // go through each pair of diff pos
+            for (int j=k+1; j<diff1.size(); j++) {
+                StringBuilder sb=new StringBuilder();
+                sb.append(u[0]);
+                sb.setCharAt(diff1.get(j), u[0].charAt(diff1.get(k)));
+                sb.setCharAt(diff1.get(k), u[0].charAt(diff1.get(j)));
+                candidates.add(sb.toString());
+            }
+        }
+        //out.println(Arrays.toString(u));
+        //out.println(diff1);
+        //out.println(candidates);
+        for (String s: candidates) {
+            int i=1;
+            for (; i<u.length; i++) {
+                int count=0;
+                for (int j=0; j<s.length(); j++) {
+                    if (s.charAt(j) !=u[i].charAt(j))
+                        count++;  
+                }
+                if (count !=0 && count !=2)
+                    break;           
+            }
+            if (i==u.length)
+                return s;
+        }
         return "-5";
     }
     static void test()
@@ -60,6 +94,10 @@ public class SwappingChar {
         in.set(1, "tetete");
         out.println(new SwappingChar(in).solve());
         in.set(0, "etetet");
+        out.println(new SwappingChar(in).solve());
+        in.set(0, "abac");
+        in.set(1, "caab");
+        in.add("acba");
         out.println(new SwappingChar(in).solve());
     }
     public static void main(String[] args)
